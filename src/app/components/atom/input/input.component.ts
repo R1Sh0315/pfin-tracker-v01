d54a2347f85eb2631db0da3199a1813daff0476c
Rishikesh Bhalekar <rishikeshbhalekar6@gmail.com>
2024-06-19 15:26:55 +0530

    working on local storage

--- src/app/components/atom/input/input.component.ts
+++ src/app/components/atom/input/input.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'pft-input',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './input.component.html',
   styleUrl: './input.component.scss'
 })
 export class InputComponent {
+  @Input() value: string = '';
+  @Input() placeholder: string = '';
+  @Output() valueChange = new EventEmitter<string>();
 
+  onInput(event: any) {
+    this.value = event.target.value;
+    this.valueChange.emit(this.value);
+  }
 }
--- src/app/components/molecule/pop-up-form/pop-up-form.component.ts
+++ src/app/components/molecule/pop-up-form/pop-up-form.component.ts
@@ -1,29 +1,52 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {AtomsModule} from '../../atom/atom.module'
+import { AtomsModule } from '../../atom/atom.module';
+import { v4 as uuidv4 } from 'uuid';
 @Component({
   selector: 'pft-pop-up-form',
   standalone: true,
   imports: [AtomsModule, CommonModule],
   templateUrl: './pop-up-form.component.html',
-  styleUrl: './pop-up-form.component.scss'
+  styleUrl: './pop-up-form.component.scss',
 })
 export class PopUpFormComponent {
   @Output() closeClicked = new EventEmitter<void>();
-  
-  isIncome:boolean =true
 
-  onClose(){
-    this.closeClicked.emit()
+  isIncome: boolean = true;
+  amount: string = '';
+  description: string = '';
+
+  onClose() {
+    this.closeClicked.emit();
   }
 
-  onClickHandler(val:string):void{
-    if(val=="Expense"){
-      this.isIncome=false
-    }else{
-      this.isIncome=true
+  onClickHandler(val: string): void {
+    if (val == 'Expense') {
+      this.isIncome = false;
+    } else {
+      this.isIncome = true;
     }
-    console.log(val)
+    console.log(val);
+  }
 
+  onSave() {
+    const data = {
+      id: uuidv4(),
+      type: this.isIncome ? 'Income' : 'Expense',
+      amount: this.isIncome ?this.amount:-this.amount,
+      description: this.description,
+    };
+    console.log(data)
+    // Store data in localStorage
+    const storedData = JSON.parse(localStorage.getItem('data') || '[]');
+    storedData.push(data);
+    localStorage.setItem('data', JSON.stringify(storedData));
+
+    // Clear inputs after save
+    this.amount = '';
+    this.description = '';
+
+    // Close popup form
+    this.closeClicked.emit();
   }
 }
